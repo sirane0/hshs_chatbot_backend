dd2aa829c9254033e769845639a5c38dbb2dff9c
sirane0 <lucy07052700@gmail.com>
2025-05-21 16:20:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,17 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 let users = [];
 
-const allowedOrigins = ["https://sirane0.github.io", "http://localhost:3000"];
-
+// ✅ GitHub Pages 도메인만 허용
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: "https://sirane0.github.io",
   credentials: true
 }));
 
